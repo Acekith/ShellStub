b8be80b176ac5a8ea97d78712a952935804bad61
Acekith <15915960+Acekith@users.noreply.github.com>
2022-11-13 17:18:38 -0600

    it parses arguments!

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ShellStub
 Stub Script for shell scripts. 
+
+
+
+# sources 
+Useful article for the cration of these scripts
+
+https://stackoverflow.com/questions/402377/using-getopts-to-process-long-and-short-command-line-options 
+https://linuxconfig.org/bash-script-flags-usage-with-arguments-examples
+https://stackoverflow.com/questions/7069682/how-to-get-arguments-with-flags-in-bash
+https://unix.stackexchange.com/questions/62950/getopt-getopts-or-manual-parsing-what-to-use-when-i-want-to-support-both-shor
+https://www.thegeekstuff.com/2010/05/bash-shell-special-parameters/
---
+++ bashbasescript.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#parse vars with getopt. 
+# NOTE: This requires GNU getopt.  On Mac OS X and FreeBSD, you have to install this
+# separately; see below.
+TEMP=$(getopt -o vda: --long verbose,debug,flagtakesargument:,noarguments,otherthing \
+              -n 'bashbasescript' -- "$@")
+
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+
+# Note the quotes around '$TEMP': they are essential!
+eval set -- "$TEMP"
+
+#declare variables and defaults. 
+VERBOSE=false
+DEBUG=false
+FLAGTAKESARGUMENT=
+JAVA_MISC_OPT=
+while true; do
+  case "$1" in
+    -v | --verbose ) VERBOSE=true; shift ;;
+    -d | --debug ) DEBUG=true; shift ;;
+    -a | --flagtakesargument ) FLAGTAKESARGUMENT="$2"; echo "mandatory argument was supplied with $FLAGTAKESARGUMENT"; shift 2 ;;
+    --noarguments)
+      echo "the noarguments switch parameter was supplied"; shift 2 ;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
+# -- catches long flags without anything after them and move to next.
+# * catches all flags given that are not supported. 
